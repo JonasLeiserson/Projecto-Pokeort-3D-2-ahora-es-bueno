d4aaf71b15a36559ef1fc051a15cf2009228b7ce
juancruzsb <sabiojuancruzsecundario@gmail.com>
2025-08-27 09:10:11 -0300

    auuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuura

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void CargarFileButtons(GameObject fileButtons, GameObject menuButtons) 
+    {
+        fileButtons.SetActive(true);
+        menuButtons.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SaveData/SaveData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class SaveData {
+    public Pokedex pokedex;
+    public Vector3 playerPosition;
+    public Inventario inventory;
+    public string saveFile;
+}
+
+public static class SaveSystem {
+    static string GetSavePath(string saveFile) {
+        string folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        string saveFolder = Path.Combine(folder, "PokeORT 3D");
+        if (!Directory.Exists(saveFolder)) Directory.CreateDirectory(saveFolder);
+        return Path.Combine(saveFolder, saveFile);
+    }
+
+    public static void SaveGame(SaveData data) {
+        string json = JsonUtility.ToJson(data); 
+        File.WriteAllText(GetSavePath(data.saveFile), json);
+    }
+
+    public static SaveData LoadGame(Button boton) {
+        string path = GetSavePath(boton.tag);
+        if (File.Exists(path)) {
+            string json = File.ReadAllText(path);
+            return JsonUtility.FromJson<SaveData>(json); 
+        }
+        return null;
+    }
+}
