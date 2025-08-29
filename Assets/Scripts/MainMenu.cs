d230d602f25d4c0681925c25b6d660abc5bb5b68
juancruzsb <sabiojuancruzsecundario@gmail.com>
2025-08-29 10:09:30 -0300

    blablabla

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,32 +10,20 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame(Button button)
     {
-        Pokedex selectedPokedex = null;
-
         switch (button.tag)
         {
             case "File 1":
                 file = GameManager.instance.file1;
-                selectedPokedex = GameManager.instance.PokedexPlayer1;
                 break;
             case "File 2":
                 file = GameManager.instance.file2;
-                selectedPokedex = GameManager.instance.PokedexPlayer2;
                 break;
             case "File 3":
                 file = GameManager.instance.file3;
-                selectedPokedex = GameManager.instance.PokedexPlayer3;
                 break;
         }
 
-        if (selectedPokedex == null)
-        {
-            Debug.LogError("Invalid button tag or uninitialized Pokedex.");
-            return;
-        }
-
         GameManager.instance.data = SaveSystem.InitSaveFileIfNeeded(file);
-        GameManager.instance.data.pokedex = selectedPokedex;
         GameManager.instance.AssignData(GameManager.instance.data);
         SceneManager.LoadScene("GameScene");
     }
