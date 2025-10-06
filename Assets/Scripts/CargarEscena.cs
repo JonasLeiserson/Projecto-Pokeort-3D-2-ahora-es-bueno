fd44123974e026e1a8b7967290a1a169376c4f0e
Jonas "El  pipa" LECHERSON <49516747@est.ort.edu.ar>
2025-10-06 10:42:25 -0300

    fog my dave

---
+++ Assets/Scripts/CargarEscena.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum Edificio
+{
+  Casa,
+  Tienda,
+  Centro
+}
+public class CargarEscena : MonoBehaviour
+{
+    public Edificio EdificioSeleccionado;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            switch (EdificioSeleccionado)
+            {
+                case Edificio.Casa:
+                    SceneManager.LoadScene("EscenaCasa");
+                    break;
+
+                case Edificio.Tienda:
+                    SceneManager.LoadScene("EscenaTienda");
+                    break;
+
+                case Edificio.Centro:
+                    SceneManager.LoadScene("EscenaCentro");
+                    break;
+            } 
+        }
+    }
+}
+    
