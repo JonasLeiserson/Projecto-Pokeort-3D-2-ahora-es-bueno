041d80980f341669eb56342aedac5bc96377ed00
Jonas "El  pipa" LECHERSON <49516747@est.ort.edu.ar>
2025-10-13 10:44:37 -0300

    totr

---
+++ Assets/Vender.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Vender : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject botonInteractuar; 
+    public GameObject jugador;  
+    public float distanciaActivacion = 3f; 
+    
+    private bool dentroRango = false;
+
+
+
+    void Start()
+    {
+        jugador = GameObject.Find("JugadorCamaraMan");
+        Debug.Log(jugador);
+        botonInteractuar = GameObject.Find("InteractuarVendedor");
+        botonInteractuar.SetActive(false);
+    }
+
+    void Update()
+    {
+        float distancia = Vector3.Distance(transform.position, jugador.position);
+
+        if (distancia <= distanciaActivacion && !dentroRango)
+        {
+            botonInteractuar.SetActive(true);
+            dentroRango = true;
+        }
+        else if (distancia > distanciaActivacion && dentroRango)
+        {
+            botonInteractuar.SetActive(false);
+            dentroRango = false;
+        }
+    }
+}
