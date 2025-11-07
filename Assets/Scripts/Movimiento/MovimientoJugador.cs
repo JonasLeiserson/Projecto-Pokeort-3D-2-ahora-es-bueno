e35c6b647393cb828f3c0a3f3ee26ccd4949f439
Jonas "El  pipa" LECHERSON <49516747@est.ort.edu.ar>
2025-11-07 10:03:47 -0300

    YA ME CANSE

--- Assets/Scripts/Movimiento/MovimientoJugador.cs
+++ Assets/Scripts/Movimiento/MovimientoJugador.cs
@@ -6,57 +6,65 @@ public class MovimientoJugador : MonoBehaviour
     public float FuerzaSalto = 8f; 
     public float Gravedad = 20f;
     private float Stamina = 100f;
-    
+    public Animator Animador; 
     public Transform CamaraPosicion; 
     private CharacterController controller;
     private Vector3 moveDirection;
 
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
         controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-            if(Stamina > 30)
+        if (Stamina > 30)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
             {
-                if(Input.GetKeyDown(KeyCode.LeftShift))
-                {
-                    MovimientoVelocidad = 10f; 
-                    Stamina -= 5;
-                }
+                MovimientoVelocidad = 10f; 
+                Stamina -= 5;
             }
+        }
 
-            if(Input.GetKeyUp(KeyCode.LeftShift))
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            MovimientoVelocidad = 5f;
+        }
+
+        transform.rotation = Quaternion.Euler(0, CamaraPosicion.eulerAngles.y, 0);
+
+        if (controller.isGrounded)
+        {
+            if (Animador.GetBool("Saltando"))
+                Animador.SetBool("Saltando", false);
+
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            Vector3 forward = CamaraPosicion.forward;
+            Vector3 right = CamaraPosicion.right;
+            forward.y = 0;
+            right.y = 0;
+            forward.Normalize();
+            right.Normalize();
+
+            Vector3 desiredMoveDirection = forward * vertical + right * horizontal;
+            moveDirection = desiredMoveDirection * MovimientoVelocidad;
+
+            if (Input.GetButtonDown("Jump"))
             {
-               MovimientoVelocidad = 5f; 
+                moveDirection.y = FuerzaSalto;
+                Animador.SetBool("Saltando", true);
             }
-            transform.rotation = Quaternion.Euler(0, CamaraPosicion.eulerAngles.y, 0);
+        }
 
-            if (controller.isGrounded)
-            {
-                float horizontal = Input.GetAxis("Horizontal");
-                float vertical = Input.GetAxis("Vertical");
+        moveDirection.y -= Gravedad * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
 
-                Vector3 forward = CamaraPosicion.forward;
-                Vector3 right = CamaraPosicion.right;
-                forward.y = 0;
-                right.y = 0;
-                forward.Normalize();
-                right.Normalize();
-
-                Vector3 desiredMoveDirection = forward * vertical + right * horizontal;
-                moveDirection = desiredMoveDirection * MovimientoVelocidad;
-
-                if (Input.GetButtonDown("Jump"))
-                {
-                    moveDirection.y = FuerzaSalto;
-                }
-            }
-
-            moveDirection.y -= Gravedad * Time.deltaTime;
-
-            controller.Move(moveDirection * Time.deltaTime);
+    public void LanzarPokebola()
+    {
+        Animador.SetBool("Lanzamiento", true);
     }
 }
