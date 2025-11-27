9ae590e8fc6a3236064217683591a96d0a4aa9a4
Juan Cruz Sabio <sabiojuancruzsecundario@gmail.com>
2025-11-27 09:21:14 -0300

    sigmanio

---
+++ Assets/BotonAceptarInicial.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class BotonAceptarInicial : MonoBehaviour
+{
+    public PokeortInstance instance;
+    public Dialogue dialogo;
+    public GameObject canva;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Aceptar()
+    {
+        UnityEngine.Debug.Log(instance);
+
+        PokedexPlayerManager.instance.pokedex.pokeorts.Add(instance);
+        canva.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        GameObject[] bolas = GameObject.FindGameObjectsWithTag("Bola");
+
+        foreach (GameObject bola in bolas)
+        {
+            Destroy(bola);
+        }
+
+        DialogueLine line1 = new DialogueLine();
+        line1.speakerName = "Darin";
+        line1.dialogueText = "¡Excelente elección! Estoy seguro de que " + instance.pokemonData.pokemonName + " y tú serán un gran equipo.";
+        dialogo.dialogueLines = new List<DialogueLine> { line1 };
+
+        DialogoManager.instance.StartDialogue(dialogo);
+
+    }
+
+
+    public void Rechazar()
+    {
+        canva.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
---
+++ Assets/PokeortInicial.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PokeortInicial : MonoBehaviour
+{
+    GameObject currentInteractable;
+    public GameObject interactButton;
+    public GameObject canva;
+    public TextMeshProUGUI pokeortName;
+    public Image pokeortImage;
+    public GameObject aceptar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentInteractable != null && !canva.activeSelf && !DialogoManager.instance.talking) 
+        {
+            interactButton.SetActive(true);
+        }
+        else
+        {
+            interactButton.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && currentInteractable != null)
+        {
+            PokeortData pokeortData = currentInteractable.GetComponent<PokemonManager>().pokemonTemplate;
+            pokeortName.text = pokeortData.pokemonName;
+            pokeortImage.sprite = pokeortData.icon;
+
+            canva.SetActive(true);
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            currentInteractable.GetComponent<PokemonManager>().Init(5);
+            PokeortInstance instance = currentInteractable.GetComponent<PokemonManager>().currentPokemonInstance;
+
+            aceptar.GetComponent<BotonAceptarInicial>().instance = instance;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            currentInteractable = gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            currentInteractable = null;
+        }
+    }
+}
